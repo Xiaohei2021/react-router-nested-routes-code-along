c0b4b3632f72687d4dfe41245cc41fafb26afc21
Leon <hualiang.tian@gmail.com>
2021-12-05 21:34:26 -0500

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,11 +1,30 @@
+// import React from "react";
+// import { Route } from "react-router-dom";
+// import MoviesList from "../components/MoviesList";
+
+// const MoviesPage = ({ movies }) => (
+//   <div>
+//     <MoviesList movies={movies} />
+//   </div>
+// );
+
+// export default MoviesPage;
+
+
 // .src/containers/MoviesPage.js
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 
+import MovieShow from "../components/MovieShow";
+
 const MoviesPage = ({ match, movies }) => (
   <div>
-    <MoviesList movies={movies} />
+    <MoviesList movies={movies} /> 
+    <Route
+      path={`${match.url}/:movieId`}
+      render={(routerProps) => <MovieShow {...routerProps} movies={movies} />}
+    />  
   </div>
 )
 
